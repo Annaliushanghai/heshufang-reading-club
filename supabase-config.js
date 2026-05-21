77bc7ae0859388e7a299c8b9fd6c520e2ca0efe9
Annaliushanghai <24790281@qq.com>
2026-05-21 10:56:03 +0800

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,4 @@
 window.HE_SHUFANG_SUPABASE = {
-  // 例如: "https://abcdxyz.supabase.co"
   url: "https://tibqzenvfvjizubbyeat.supabase.co",
-  // 使用 Publishable key（sb_publishable_...）
   anonKey: "sb_publishable_pMlxEDUrQ1_6IJ3XUf62Sw_BBif_vgn"
 };
