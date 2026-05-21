3b05fe75b82bd9d1af8e9dc040cfbbe1e92526c2
Annaliushanghai <24790281@qq.com>
2026-05-21 11:39:46 +0800

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.HE_SHUFANG_SUPABASE = {
-  url: "https://tibqzenvfvjizubbyeat.supabase.co",
-  anonKey: "sb_publishable_pMlxEDUrQ1_6IJ3XUf62Sw_BBif_vgn"
+  url: "https://jikyemqfhbapoaavscbn.supabase.co",
+  anonKey: "sb_publishable_ChI_VOAvOWajfPz-K2aUCQ_IOZP47e-"
 };
